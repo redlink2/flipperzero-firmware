57d7d4a12603c15729e6378b6f6ab27034b4666e
Ashton Redd <redlink2@pm.me>
2022-05-16 23:42:51 -0500

    Added level_up plugin -from muddledbox with tweaks

--- applications/applications.c
+++ applications/applications.c
@@ -51,6 +51,7 @@ extern int32_t snake_game_app(void* p);
 extern int32_t tetris_game_app(void* p);
 extern int32_t spectrum_analyzer_app(void* p);
 extern int32_t clock_app_old(void* p);
+extern int32_t level_up_app(void* p);
 
 // On system start hooks declaration
 extern void bt_on_system_start();
@@ -375,6 +376,15 @@ const FlipperApplication FLIPPER_PLUGINS[] = {
      .stack_size = 1024,
      .icon = &A_Games_14,},
 #endif
+
+#ifdef APP_LEVEL_UP
+    {.app = level_up_app,
+     .name = "Dolphin EXP",
+     .stack_size = 1024,
+     .icon = &A_Plugins_14,
+     .flags = FlipperApplicationFlagDefault},
+#endif
+
 };
 
 const size_t FLIPPER_PLUGINS_COUNT = COUNT_OF(FLIPPER_PLUGINS);
