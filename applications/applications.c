afdd84580b8ae941b85797c9470a91cd2a5dc8ce
Ashton Redd <redlink2@pm.me>
2022-05-18 19:15:25 -0500

    Added animation for snake -custom made in aseprite by me -plans to move the pellet more center

--- applications/applications.c
+++ applications/applications.c
@@ -313,6 +313,14 @@ const FlipperApplication FLIPPER_APPS[] = {
      .flags = FlipperApplicationFlagDefault},
 #endif
 
+#ifdef APP_SNAKE_GAME
+    {.app = snake_game_app,
+     .name = "Snake Game",
+     .stack_size = 1024,
+     .icon = &A_Snake_14,
+     .flags = FlipperApplicationFlagDefault},
+#endif
+
 };
 
 const size_t FLIPPER_APPS_COUNT = COUNT_OF(FLIPPER_APPS);
@@ -375,14 +383,6 @@ const FlipperApplication FLIPPER_PLUGINS[] = {
      .name = "Bluetooth Remote",
      .stack_size = 1024,
      .icon = NULL,
-     .flags = FlipperApplicationFlagDefault},
-#endif
-
-#ifdef APP_SNAKE_GAME
-    {.app = snake_game_app,
-     .name = "Snake Game",
-     .stack_size = 1024,
-     .icon = &A_Games_14,
      .flags = FlipperApplicationFlagDefault},
 #endif
 
